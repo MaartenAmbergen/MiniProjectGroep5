bded3bbf2cb401794e4f7473c6f6ea1dfff0a942
MaartenAmbergen <31821184+MaartenAmbergen@users.noreply.github.com>
2018-10-22 15:19:22 +0200

    Update AlarmV4.py

--- AlarmV4.py
+++ AlarmV4.py
@@ -1,6 +1,14 @@
 from gpiozero import LED, Button
-from signal import pause
 from time import sleep
+import socket
+
+HOST = '145.89.154.183' # Enter IP or Hostname of your server
+PORT = 12345 # Pick an open Port (1000+ recommended), must match the server port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+#Lets loop awaiting for your input
+
 
 ledG = LED(17)
 ledY = LED(27)
@@ -13,8 +21,8 @@ def Alarmtriggered():
     ledG.off()
     ledY.on()
     tijd = 0
-    while tijd <500:
-        tijd = tijd+1
+    while tijd < 500:
+        tijd = tijd + 1
         if button2.is_pressed:
             AlarmAan()
             break
@@ -22,6 +30,9 @@ def Alarmtriggered():
     ledR.on()
     ledY.off()
     ledG.off()
+    command = raw_input('Enter your command: ')
+    s.send(command)
+
 
 def AlarmAan():
     ledG.on()
@@ -29,8 +40,6 @@ def AlarmAan():
     while True:
         if button1.is_pressed:
             Alarmtriggered()
-            
-AlarmAan()
-if button1.is_pressed and button2.is_pressed:
-    exit()
 
+
+AlarmAan()
